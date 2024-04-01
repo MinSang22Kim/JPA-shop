de87ed048c9997e411e9556923b5d5d5a39efe3e
MinSang22Kim <powerminsang9901@gmail.com>
2024-04-01 13:40:33 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -17,8 +21,9 @@ public abstract class Item {
     private Long id;
 
     private String name;
-
     private int price;
-
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
