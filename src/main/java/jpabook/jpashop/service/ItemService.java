8f2ac85a71abcbbf050a42c44b4049775b05eea3
MinSang22Kim <powerminsang9901@gmail.com>
2024-04-06 19:38:34 +0900

    변경 감지와 병합

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,15 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    // 변경감지 방법으로 준영속 엔티티 수정
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) { // 피라미터로 넘어온 준영속 상태의 엔티티
+        Item findItem = itemRepository.findOne(itemId); // 같은 엔티티를 조회
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity); // 엔티티 수정
     }
 
     public List<Item> findItems() {
